1d2ec3b4881e2bb8b16b292af4f1b2c2ad23a181
stainedrug <mike.r.jenkins.jr@gmail.com>
2023-02-02 14:43:57 -0500

    Update vcCredentials.groovy

--- vars/vcCredentials.groovy
+++ vars/vcCredentials.groovy
@@ -1,7 +1,7 @@
 import groovy.transform.Field
 vars/vcCredentials.groovy
 
-@Field credentialsId = ecurity-API-user
+@Field credentialsId = 'ecurity-API-user'
 
 def credentialsId() {
     echo "ecurity-API-user"
