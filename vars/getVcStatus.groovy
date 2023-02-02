3133dea4ecc8f330f0569429aad14406b4b4d120
stainedrug <mike.r.jenkins.jr@gmail.com>
2023-02-02 09:13:43 -0500

    Update getVcStatus.groovy

--- vars/getVcStatus.groovy
+++ vars/getVcStatus.groovy
@@ -1,11 +1,1 @@
-class Student {
-   int StudentID;
-   String StudentName;
-	
-   static void main(String[] args) {
-      Student st = new Student();
-      st.StudentID = 1;
-      st.StudentName = "Joe"
-      return st.StudentName;	
-   } 
-}
+
