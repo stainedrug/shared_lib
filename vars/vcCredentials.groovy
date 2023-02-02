9b15ac835bb3a884143010c446dd312632763c96
stainedrug <mike.r.jenkins.jr@gmail.com>
2023-02-02 09:35:50 -0500

    Update vcCredentials.groovy

---
+++ vars/vcCredentials.groovy
@@ -0,0 +1,13 @@
+vars/vcCredentials.groovy
+
+def credentialsId() {
+    echo "ecurity-API-user"
+}
+
+def usernameVariable() {
+    echo "security_API_user"
+}
+
+def passwordVariable() {
+echo "security_API_pass"
+}
