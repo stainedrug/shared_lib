4264624f3315e2533ff808c8288b44ce64ab4c39
Michael Jenkins <mjenkins24@dxc.com>
2023-02-21 09:24:18 -0500

    dockerfile build

--- vars/dockerContents.groovy
+++ vars/dockerContents.groovy
@@ -2,5 +2,8 @@ import groovy.transform.Field
 vars/dockerContents.groovy
 
 @Field dockerFileText = "
-FROM openjdk:8-jdk-alpine\nRUN apk add --no-cache curl zip bash\nRUN curl -s -o /tmp/veracode.zip https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/19.3.5.7/vosp-api-wrappers-java-19.3.5.7-dist.zip\nRUN cd /tmp && unzip -o veracode.zip VeracodeJavaAPI.jar && mv VeracodeJavaAPI.jar /
+FROM openjdk:8-jdk-alpine
+RUN apk add --no-cache curl zip bash
+RUN curl -s -o /tmp/veracode.zip https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/19.3.5.7/vosp-api-wrappers-java-19.3.5.7-dist.zip
+RUN cd /tmp && unzip -o veracode.zip VeracodeJavaAPI.jar && mv VeracodeJavaAPI.jar /
 "
