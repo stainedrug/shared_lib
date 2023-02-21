56a3e18e9021171987c12ef293a402811ecb0bdc
Michael Jenkins <mjenkins24@dxc.com>
2023-02-21 09:34:52 -0500

    dockerfile build

--- vars/buildDockerFile.groovy
+++ vars/buildDockerFile.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo 'hello1'
     def dockerFileContent = dockerContents.dockerFileText
-    echo 'hello2'
-    writeFile file:"testfolder/Dockerfile", text: dockerFileContent
+    writeFile file:"./testfolder/Dockerfile", text: dockerFileContent
     sh "chmod a+x ./testfolder/Dockerfile"
 }
