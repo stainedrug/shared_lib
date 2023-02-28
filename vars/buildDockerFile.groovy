f11e4227b1899ac34fd5a63ecb8ab1a53ae124e6
stainedrug <mike.r.jenkins.jr@gmail.com>
2023-02-28 09:07:17 -0500

    Delete buildDockerFile.groovy

--- vars/buildDockerFile.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-    def dockerFileContent = dockerContents.dockerFileText
-    writeFile file:"./testfolder/Dockerfile", text: dockerFileContent
-    sh "chmod a+x ./testfolder/Dockerfile"
-}
