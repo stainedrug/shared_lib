2450a8ac269840fdbf038f7d38adeb8ae4dc59d1
stainedrug <mike.r.jenkins.jr@gmail.com>
2024-07-30 15:11:49 -0400

    Update csCredentials.groovy

--- vars/csCredentials.groovy
+++ vars/csCredentials.groovy
@@ -1,6 +1,6 @@
 import groovy.transform.Field
 vars/csCredentials.groovy
 
-@Field credentialsId = "'pdxc_checkmarx'";
-@Field usernameVariable = "'security_user'";
-@Field passwordVariable = "'security_pass'";
+@Field credentialsId = 'pdxc_checkmarx';
+@Field usernameVariable = 'security_user';
+@Field passwordVariable = 'security_pass';
