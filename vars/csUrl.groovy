67a8c5179a010b148611301ec3322b2615c4e2d9
Michael Jenkins <mjenkins24@dxc.com>
2024-07-30 11:47:13 -0400

    change filnames

---
+++ vars/csUrl.groovy
@@ -0,0 +1,4 @@
+import groovy.transform.Field
+vars/vcUrl.groovy
+
+@Field vcUrl = "https://analysiscenter.veracode.com/api/4.0/summaryreportpdf.do?build_id="
--- vars/vcCredentials.groovy
+++
@@ -1,6 +0,0 @@
-import groovy.transform.Field
-vars/vcCredentials.groovy
-
-@Field credentialsId = "'SEC_API_ID'";
-@Field usernameVariable = "'SEC_API_USR'";
-@Field passwordVariable = "'SEC_API_PAS'";
