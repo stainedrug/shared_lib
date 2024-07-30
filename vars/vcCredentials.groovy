86051c5fd6057d2468707b8afaf87eaffbc1cd81
stainedrug <mike.r.jenkins.jr@gmail.com>
2024-07-30 11:41:21 -0400

    Update vcCredentials.groovy

--- vars/vcCredentials.groovy
+++ vars/vcCredentials.groovy
@@ -1,6 +1,6 @@
 import groovy.transform.Field
 vars/vcCredentials.groovy
 
-@Field credentialsId = "'SEC_API_USR'";
+@Field credentialsId = "'SEC_API_ID'";
 @Field usernameVariable = "'SEC_API_USR'";
 @Field passwordVariable = "'SEC_API_PAS'";
